03e922844c7ec79f146f56005d5840887711a498
Sondre Lindkjolen <sondredl@gmail.com>
2024-11-06 06:49:59 +0100

    restores changes

--- editor/debugger/editor_debugger_server.cpp
+++ editor/debugger/editor_debugger_server.cpp
@@ -46,12 +46,12 @@ private:
 public:
 	static EditorDebuggerServer *create(const String &p_protocol);
 
-	void poll() override {}
+	virtual void poll() override {}
 	virtual String get_uri() const override;
 	virtual Error start(const String &p_uri) override;
-	void stop() override;
-	bool is_active() const override;
-	bool is_connection_available() const override;
+	virtual void stop() override;
+	virtual bool is_active() const override;
+	virtual bool is_connection_available() const override;
 	virtual Ref<RemoteDebuggerPeer> take_connection() override;
 
 	EditorDebuggerServerTCP();
